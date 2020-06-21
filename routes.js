baee807b9affb6b2b3454cc0cbde2bd30ea32426
Alisson Medeiros <alisilva1@gmail.com>
2020-06-21 18:02:19 -0300

    finalizando cadastro de professor

--- routes.js
+++ routes.js
@@ -13,6 +13,9 @@ routes.get('/professor/create', function(req,res){
     return res.render("professor/create")
 })
 
+routes.post('/professor',function(req,res){
+    return res.send("Recebido!")
+})
 
 routes.get('/student', function(req,res){
     return res.send("/student")
