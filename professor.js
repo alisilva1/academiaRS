0dff3e523c4ca523c2f7003a5d115d409c092fba
Alisson Medeiros <alisilva1@gmail.com>
2020-06-22 21:41:01 -0300

    Ajuste campos do data

--- professor.js
+++ professor.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const data = require("./data.json");
 //create
 exports.post = function(req,res){
     const keys = Object.keys(req.body)
@@ -5,8 +7,16 @@ exports.post = function(req,res){
         if(req.body[key] == "")
             return res.send("Por favor preencha todos os campos!")
     }
+    req.body.birth = Date.parse(req.body.birth)
+    req.body.criado_em = Date.now()
 
-    return res.send(req.body)
+    data.professor.push(req.body)
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 4),function(err){
+        if(err) return res.send("O preenchimento dos dados está errado.")
+
+        return res.redirect("/professor")
+    } )
 }
 
 //update
